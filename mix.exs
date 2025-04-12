bc315b01b5623d160495b57e0c3b639fc99f3ffe
Antonio Toncetti <antonio.toncetti@gmail.com>
2025-04-12 12:54:40 +0200

    Mirror lago schema version.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule LagoClient.MixProject do
   def project do
     [
       app: :lago_client,
-      version: "0.1.0",
+      version: "1.22.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps()
